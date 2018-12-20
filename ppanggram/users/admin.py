205505dc2a847f1c2c44c7b9b7a5d945da31ef39
황채환 <chaehwanhwang@hwangchaehwan-ui-MacBookAir.local>
2018-12-20 22:57:01 +0900

    django-admim Custom, users admin Noamrd code copy

--- ppanggram/users/admin.py
+++ ppanggram/users/admin.py
@@ -1,17 +1,44 @@
 from django.contrib import admin
 from django.contrib.auth import admin as auth_admin
 from django.contrib.auth import get_user_model
-
 from ppanggram.users.forms import UserChangeForm, UserCreationForm
+
+from django import forms
+from .models import User
 
 User = get_user_model()
 
+# 유저 업데이트 사용하는 폼
+class MyUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = User
+
+
+class MyUserCreationForm(UserCreationForm):
+
+    error_message = UserCreationForm.error_messages.update({
+        'duplicate_username': 'This username has already been taken.'
+    })
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+
+    def clean_username(self):
+        username = self.cleaned_data["username"]
+        try:
+            User.objects.get(username=username)
+        except User.DoesNotExist:
+            return username
+        raise forms.ValidationError(self.error_messages['duplicate_username'])
+# 여기 까지유저 업데이트 할때 사용하는 폼
 
 @admin.register(User)
 class UserAdmin(auth_admin.UserAdmin):
 
     form = UserChangeForm
     add_form = UserCreationForm
-    fieldsets = (("User", {"fields": ("name",)}),) + auth_admin.UserAdmin.fieldsets
+    fieldsets = (
+        ("User", {"fields": ("name", "followers", "following")}),
+    ) + auth_admin.UserAdmin.fieldsets
     list_display = ["username", "name", "is_superuser"]
     search_fields = ["name"]
