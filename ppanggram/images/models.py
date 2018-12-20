71c5c6488c10738da4ff4c5cc29d280896b17b8e
황채환 <chaehwanhwang@hwangchaehwan-ui-MacBookAir.local>
2018-12-20 20:27:41 +0900

    creating the Image Model

--- ppanggram/images/models.py
+++ ppanggram/images/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class TimeStampedModel(models.Model):
+
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+# class Meta는 반복을 피하기 위한 참조값
+
+class Image(TimeStampedModel):
+
+    file = models.ImageField()
+    location = models.CharField(max_length=140)
+    caption = models.TextField()
+
+# 각 값들이 어떤 데이터를 저장하는지 알려주고
+# 이미 존재하고 있지 않은 값들이 때문에 null=True는 필요하지 않음
+
+class Comment(TimeStampedModel):
+
+    message = models.TextField()
+
