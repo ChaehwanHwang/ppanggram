24234122f663b6ed4f2b78cde36026579c3ef7f8
ChaehwanHwang <coghks89@gmail.com>
2018-12-21 22:39:51 +0900

    serializers create (image, comment, like)

--- config/settings/base.py
+++ config/settings/base.py
@@ -67,7 +67,7 @@ THIRD_PARTY_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    'rest_framework',
+    'rest_framework', # 파이썬 오브젝트를 제이슨 오브젝트로 변경 반대도 마찬가지
 ]
 LOCAL_APPS = [
     'ppanggram.users.apps.UsersAppConfig',
