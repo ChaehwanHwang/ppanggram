0b08a8bf6d267c1ee4b7e1010091944c6f3c5666
ChaehwanHwang <coghks89@gmail.com>
2019-01-09 22:49:50 +0900

    delete comment

--- ppanggram/images/urls.py
+++ ppanggram/images/urls.py
@@ -10,15 +10,21 @@ urlpatterns = [
     ),
 
     url(
-        regex=r'(?P<image_id>\w+)/like/',
+        regex=r'^(?P<image_id>\w+)/likes/$',
         view=views.LikeImage.as_view(),
         name='like_Image'
     ),
 
     url(
-        regex=r'(?P<image_id>\w+)/comment/',
+        regex=r'^(?P<image_id>\w+)/comments/$',
         view=views.CommentOnImage.as_view(),
         name='comment_Image'
+    ),
+
+    url(
+        regex=r'comments/(?P<comment_id>[0-9]+)/$',
+        view=views.Comment.as_view(),
+        name='comment'
     )
 ]
 
