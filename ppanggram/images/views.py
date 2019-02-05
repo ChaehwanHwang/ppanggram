18d6f3b0c95015bc5d3935391f42e42c484d3416
ChaehwanHwang <coghks89@gmail.com>
2019-02-05 20:04:14 +0900

    image delete

--- ppanggram/images/views.py
+++ ppanggram/images/views.py
@@ -206,6 +206,15 @@ class ModerateComments(APIView):
 
 class ImageDetail(APIView):
 
+    def find_own_image(self, image_id, user):
+        
+        try:
+            image = models.Image.objects.get(id=image_id, creator=user)
+            return image
+        except models.Image.DoesNotExist:
+            return None
+
+
     def get(self, request, image_id, format=None):
 
         user = request.user
@@ -219,14 +228,17 @@ class ImageDetail(APIView):
 
         return Response(data=serializer.data, status=status.HTTP_200_OK)
 
+
     def put(self, request, image_id, format=None):
 
         user = request.user
 
-        try:
-            image = models.Image.objects.get(id=image_id, creator=user)
-        except models.Image.DoesNotExist:
+        image = self.find_own_image(image_id, user)
+
+        if image is None:
+
             return Response(status=status.HTTP_401_UNAUTHORIZED)
+
 
         serializer = serializers.InputImageSerializer(image, data=request.data, partial=True)
 
@@ -239,6 +251,20 @@ class ImageDetail(APIView):
         else:
             return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+    def delete(self, request, image_id, format=None):
+
+        user = request.user
+
+        image = self.find_own_image(image_id, user)
+
+        if image is None:
+
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+        image.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 
