2028e049f402cef2bfd1f92bd49d4b342cd9b6fd
ChaehwanHwang <coghks89@gmail.com>
2019-02-28 22:18:22 +0900

    Creating the User Redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+localStorage.setItem('ppang', 'ppangAcademy')
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
